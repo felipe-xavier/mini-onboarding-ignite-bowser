aec3a5f0ae35f78cf9d33237e55d542f1fad9304
felipe.xavier <felipe.xavier@funceme.br>
2020-11-05 19:28:23 -0300

    User the new presets for text.

--- app/screens/landing-screen/landing-screen.tsx
+++ app/screens/landing-screen/landing-screen.tsx
@@ -3,7 +3,7 @@ import { observer } from "mobx-react-lite"
 import { StyleSheet, SafeAreaView, View } from "react-native"
 import RadioButtonRN from 'radio-buttons-react-native'
 import { Screen, Text, Wallpaper, Button } from "../../components"
-import { color, typography } from "../../theme"
+import { color, spacing } from "../../theme"
 import { useNavigation } from "@react-navigation/native"
 import { useStores } from "../../models"
 
@@ -26,36 +26,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  continueText: {
-    color: color.palette.white,
-    fontFamily: typography.primary,
-    fontSize: 16,
-    fontWeight: "bold",
-    letterSpacing: 2,
-  },
   radioBtn: {
-    padding: 16
+    padding: spacing[4]
   },
   radioBtnItemText: {
     fontSize: 18,
-    paddingVertical: 12
+    paddingVertical: spacing[2]
   },
-  // eslint-disable-next-line react-native/no-color-literals
   radioBtnView: {
-    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    backgroundColor: color.palette.lowWhite,
+    paddingHorizontal: spacing[4],
   },
-  // eslint-disable-next-line react-native/no-color-literals
-  radioBtnViewSubTitleText: {
-    color: '#3f3f3f', textAlign: 'center'
-  },
-  // eslint-disable-next-line react-native/no-color-literals
-  radioBtnViewTitleText: {
-    color: '#3f3f3f',
-    fontSize: 24,
-    paddingVertical: 8,
-    textAlign: 'center'
-  },
-  // eslint-disable-next-line react-native/no-color-literals
 })
 
 export const LandingScreen = observer(function GoalsScreen() {
@@ -78,8 +59,8 @@ export const LandingScreen = observer(function GoalsScreen() {
       <Wallpaper />
       <SafeAreaView style={styles.bottom}>
         <View style={ styles.radioBtnView}>
-          <Text style={ styles.radioBtnViewTitleText } text="What are your goals?"/>
-          <Text style={ styles.radioBtnViewSubTitleText } text="Help us tailor our program to your needs"/>
+          <Text preset={"title"} text="What are your goals?"/>
+          <Text preset={"subtitle"} text="Help us tailor our program to your needs"/>
           <RadioButtonRN
             data={goals}
             selectedBtn={onClick}
@@ -92,7 +73,6 @@ export const LandingScreen = observer(function GoalsScreen() {
           />
         </View>
         <Button
-          textStyle={ styles.continueText }
           tx="welcomeScreen.continue"
           onPress={nextScreen}
           preset={onBoardingStore.goal ? "continue" : "disabled"}
