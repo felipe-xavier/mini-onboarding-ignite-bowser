a8c130a2c472ce55ea8dc6bd17ac166c38ee0f3f
felipe.xavier <felipe.xavier@funceme.br>
2020-11-05 19:56:47 -0300

    Remove unnecessary files.

--- storybook/storybook-registry.ts
+++
@@ -1,5 +0,0 @@
-require("../app/components/text/text.story")
-require("../app/components/button/button.story")
-require("../app/components/wallpaper/wallpaper.story")
-require("../app/components/icon/icon.story")
-require("../app/components/header/header.story")
